dc1bfa2feca564aa3f424f9a5933f6a165a54937
KaranB37 <86348214+KaranB37@users.noreply.github.com>
2024-10-14 01:38:30 +0530

    new 1

--- src/app/product/page.jsx
+++ src/app/product/page.jsx
@@ -3,8 +3,6 @@ import { getProducts } from 'src/actions/product-ssr';
 
 import { ProductShopView } from 'src/sections/product/view';
 
-// ----------------------------------------------------------------------
-
 export const metadata = { title: `Product shop - ${CONFIG.appName}` };
 
 export default async function Page() {
